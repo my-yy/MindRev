f7f46ff9d7da89a30112ddb0d437335d05639bf4
xxx <xxx@xxx.com>
2023-09-17 22:25:44 +0800

    update config

--- 1_make_pkg_mac.sh
+++ 1_make_pkg_mac.sh
@@ -2,4 +2,5 @@
 rm -rf ./dist_electron/*
 export PYTHON_PATH="/Users/my/.pyenv/versions/2.7.18/bin/python"
 npm run build_mac
+npm run build_win
 open dist_electron
--- 2_make_pkg_win.sh
+++ 2_make_pkg_win.sh
@@ -1,3 +1,4 @@
+#这个脚本在windows下执行
 #务必删除里面的内容，以防上传错误
 rm -rf ./dist_electron/*
 npm run build_mac
